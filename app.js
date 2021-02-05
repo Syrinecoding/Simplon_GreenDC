2a7abc40079350f0b7d7e8980c1d460f62e18e55
NADIA224 <77812383+NADIA224@users.noreply.github.com>
2021-02-05 14:17:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('nav');
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('menu-open');
+});
+
